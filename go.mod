3bbb2b8582ddb7ad0cbe90f8ee82ceac4d5beefc
Jadon <10365495-wolffs@users.noreply.gitlab.com>
2023-07-03 13:43:40 +0200

    chore: update with code from monorepo

--- go.mod
+++ go.mod
@@ -4,22 +4,20 @@ go 1.19
 
 require (
 	github.com/google/uuid v1.3.0
-	github.com/stretchr/testify v1.8.1
-	github.com/wolffshots/phocus_crc v0.0.0-20230426105240-c3f33a1eb597
-	github.com/wolffshots/phocus_mqtt v0.0.0-20230426105149-7f39b6f21cd2
-	github.com/wolffshots/phocus_serial v0.0.0-20230426105436-e8a7e9fff872
+	github.com/stretchr/testify v1.8.3
+	github.com/wolffshots/phocus/v2 v2.3.0
 )
 
 require (
 	github.com/creack/goselect v0.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/eclipse/paho.mqtt.golang v1.4.2 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sigurn/crc16 v0.0.0-20211026045750-20ab5afb07e3 // indirect
-	go.bug.st/serial v1.4.0 // indirect
-	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
+	go.bug.st/serial v1.5.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- messages.go
+++ messages.go
@@ -4,8 +4,11 @@
 package phocus_messages
 
 import (
-	"github.com/google/uuid"
 	"log"
+
+	"github.com/google/uuid"
+	phocus_serial "github.com/wolffshots/phocus/v2/serial"
+	// comms with inverter
 )
 
 // Message is the shape of a message for phocus to interpret and handle queuing of
@@ -15,32 +18,41 @@ type Message struct {
 	Payload string    `json:"payload"`
 }
 
+// Response is the generic response from a Message
+type Response struct {
+	QPGSnResponse
+}
+
+var Port phocus_serial.Port
+
 // Interpret converts the generic `phocus` message into a specific inverter message
 // TODO add even more generalisation and separated implementation details here
-func Interpret(input Message) error {
+func Interpret(port phocus_serial.Port, input Message) (*Response, error) {
 	switch input.Command {
 	case "QPGS1":
-		err := HandleQPGS(1)
+		response, err := HandleQPGS(port, 1)
 		if err != nil {
 			log.Printf("Failed to handle %s :%v\n", input.Command, err)
 		}
-		return err
+		if response == nil {
+			return nil, err
+		} else {
+			return &Response{QPGSnResponse: *response}, err
+		}
 	case "QPGS2":
-		err := HandleQPGS(2)
+		response, err := HandleQPGS(port, 2)
 		if err != nil {
 			log.Printf("Failed to handle %s :%v\n", input.Command, err)
 		}
-		return err
+		if response == nil {
+			return nil, err
+		} else {
+			return &Response{QPGSnResponse: *response}, err
+		}
 	case "QID":
 		log.Println("TODO send QID")
 	default:
 		log.Println("Unexpected message on queue")
 	}
-	return nil
-}
-
-// Command interface is a WIP
-type Command interface {
-	New()
-	Print()
+	return nil, nil
 }
